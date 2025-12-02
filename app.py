9da4bf47f7e7337d963ed0b3c13eb87d664f711d
Udara Jayawardena <udarajayawardena5@gmail.com>
2025-12-02 11:38:41 +0530

    feat: added-post-get-apis-formatted

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import datetime, timedelta
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)   
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self):
+        return '<Task $r>' % self.id
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue adding your task"
+    else:  
+         task = Todo.query.order_by(Todo.date_created).all()
+         return render_template("index.html", tasks=task)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+        
+# app.secret_key = "flask_secret_key"
+# app.permanent_session_lifetime = timedelta(minutes=5)
+
+
+# def index():
+#     if request.method == "POST":
+#         task_content = request.form['content']
+#         new_task = Todo(content=task_content)
+#         try:
+#             db.session.add(new_task)
+#             db.session.commit()
+#             return redirect("/")
+#         except:
+#             return "There was an issue adding your task"
+#     else:  
+#          task = Todo.query.order_by(Todo.date_created).all()
+#          return render_template("index.html", tasks=task)
